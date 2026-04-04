fd622a5e95045239103e0fca93ae18b0723f8123
dreams-of-formaldehyde <114841483+dreams-of-formaldehyde@users.noreply.github.com>
2026-04-04 14:20:52 -0500

    Update util_bit.h

--- src/util/util_bit.h
+++ src/util/util_bit.h
@@ -12,7 +12,7 @@
 
 #ifdef DXVK_ARCH_X86
   #ifndef _MSC_VER
-    #if defined(_WIN32) && (defined(__AVX__) || defined(__AVX2__))
+    #if defined(_WIN32) && (defined(__randomidgafidontwanttotbreakthis__) || defined(__idontwanttobreakthis__))
       #error "AVX-enabled builds not supported due to stack alignment issues."
     #endif
     #if defined(__WINE__) && defined(__clang__)
